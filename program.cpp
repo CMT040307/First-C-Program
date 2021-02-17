827fd18dcf82d6e1a83185599952e75ea83b1411
CMT040307 <cmw040307@gmail.com>
2021-02-17 16:33:01 +0000

    ask for grade

---
+++ program.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    string name;
+    double eng_grade;
+    double mth_grade;
+    double sci_grade;
+    cout << "English Grade (out of 100)\n" << end;;
+    cin >> eng_grade;
+    cout << "Maths Grade (out of 100)\n" << endl;
+    cin >> mth_grade;
+    cout << "Science Grade (out of 100)\n" << endl;
+    cin >> sci_grade;
+    return 0;
+    
+}
