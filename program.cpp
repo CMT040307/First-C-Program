6bda7347b4d84449ce7fd093fde6951ff747ac04
CMT040307 <cmw040307@gmail.com>
2021-02-18 13:41:33 +0000

    test if passed or not

--- program.cpp
+++ program.cpp
@@ -14,6 +14,25 @@ int main()
     cin >> mth_score;
     cout << "Science score (out of 100)\n" << endl;
     cin >> sci_score;
+    
+    if(eng_score >= 85){
+        cout << "You have passed english!" << endl;
+    } else {
+        cout << "You have not passed english!" << endl;
+    }
+
+    if(mth_score >= 85){
+        cout << "You have passed maths!" << endl;
+    } else {
+        cout << "You have not passed maths!" << endl;
+    }
+
+    if(sci_score >= 85){
+        cout << "You have passed science!" << endl;
+    } else {
+        cout << "You have not passed science!" << endl;
+    }
+    
     return 0;
     
 }
